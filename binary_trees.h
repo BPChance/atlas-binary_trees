bcdc03ed76ded9e9f1f7198168990ed91ac95173
root <bradenpchance6@gmail.com>
2024-04-16 14:42:50 +0000

    added function traverses binary trees inorder

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,7 @@
 #define BINARY_H
 
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -33,5 +33,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
