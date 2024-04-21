accf99a0813cf4730014dad3ae9ab77745b12e96
root <bradenpchance6@gmail.com>
2024-04-21 05:36:02 +0000

    binary_tree_is_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,20 +6,27 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
-	int left_perfect = 0, right_perfect = 0;
+	size_t height = 0, count = 0, perfect_nodes;
+	const binary_tree_t *current = tree;
 
 	if (tree == NULL)
-		return 1;
+		return (0);
+	
+	while (current)
+	{
+		height++;
+		current = current->left;
+	}
 
-	/* Count the height of the left and right subtrees */
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	current = tree;
 
-	/* Check if left and right subtrees are perfect */
-	left_perfect = binary_tree_is_perfect(tree->left);
-	right_perfect = binary_tree_is_perfect(tree->right);
+	while (current)
+	{
+		count++;
+		current = current->right;
+	}
 
-	/* Check if both subtrees have the same height and are perfect */
-	return ((left_height == right_height) && left_perfect && right_perfect);
+	perfect_nodes = (1 << height) - 1;
+
+	return (count == perfect_nodes);
 }
